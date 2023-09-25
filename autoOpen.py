d0cfeeb51e5aa37ec47a7a0e7d889a03b4a82aba
AmkoAmin <146035902+AmkoAmin@users.noreply.github.com>
2023-09-26 00:15:05 +0200

    Add files via upload

---
+++ autoOpen.py
@@ -0,0 +1,41 @@
+import pyautogui
+import time
+
+pyautogui.hotkey('win', 's')
+
+time.sleep(1)
+
+pyautogui.hotkey('c')
+pyautogui.hotkey('h')
+pyautogui.hotkey('r')
+pyautogui.hotkey('o')
+pyautogui.hotkey('m')
+pyautogui.hotkey('e')
+
+pyautogui.hotkey('enter')
+time.sleep(1)
+pyautogui.hotkey('tab')
+pyautogui.hotkey('enter')
+
+pyautogui.hotkey('d')
+pyautogui.hotkey('b')
+pyautogui.hotkey('i')
+pyautogui.hotkey('l')
+pyautogui.hotkey('d')
+pyautogui.hotkey('u')
+pyautogui.hotkey('n')
+pyautogui.hotkey('g')
+pyautogui.hotkey('s')
+pyautogui.hotkey('c')
+pyautogui.hotkey('l')
+pyautogui.hotkey('o')
+pyautogui.hotkey('u')
+pyautogui.hotkey('d')
+pyautogui.hotkey('.')
+pyautogui.hotkey('d')
+pyautogui.hotkey('e')
+
+pyautogui.hotkey('enter')
+
+exit()
+
